eb488734f00b00945714ec2ebf8d28b0366da918
makotarifa <angeljesusmorgallego@gmail.com>
2024-08-05 04:03:02 +0200

    Added First Most Popular Courses section.

---
+++ src/views/sections/HomeView/FirstMostPopularCourses.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+import FmpCard from "@/views/sections/HomeView/FmpCard.vue";
+import fmpcData from "@/data/fmpcData.json";
+import { ref } from "vue";
+
+interface FMPCardEntity {
+    title: string;
+    description: string;
+    image: string;
+}
+
+const fmpcList = ref<FMPCardEntity[]>(fmpcData);
+const cardWidth = "329px";
+</script>
+
+<template>
+    <section class="first-most-popular">
+        <div>
+            <div>
+                <h6>Practice Advice</h6>
+                <h3>Most Popular Courses</h3>
+                <p>Problems trying to resolve the conflict between</p>
+                <p>the two major realms of Classical physics: Newtonian mechanics</p>
+            </div>
+            <div class="cards-container">
+                    <fmp-card
+                        v-for="fmpc in fmpcList"
+                        :title="fmpc.title"
+                        :description="fmpc.description"
+                        :image="fmpc.image"
+                        :width="cardWidth"
+                    />
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+.first-most-popular {
+    display: flex;
+    justify-content: center;
+}
+
+.first-most-popular > div {
+    color: var(--light-primary-text-color);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    text-align: center;
+    padding: 160px 0;
+    gap: 96px;
+    max-width: 1050px;
+}
+
+.first-most-popular > div > div:nth-child(1) {
+    padding: 45px 0;
+}
+
+.first-most-popular h3 {
+    padding: 1rem 0;
+}
+
+.first-most-popular .cards-container{
+    display: flex;
+    gap: 30px;
+    justify-content: center;
+    align-items: center;
+}
+
+
+</style>
---
+++ src/views/sections/HomeView/FmpCard.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+
+const props = defineProps<{
+    image: string;
+    description: string;
+    title: string;
+    width: string;
+}>();
+
+</script>
+<template>
+    <div class="fmp-card" :style="{ width: props.width }">
+        <div class="img-controller">
+            <img :src="props.image" />
+        </div>
+        <hr class="line-separator" />
+        <h5>{{props.title}}</h5>
+        <p>{{ props.description }}</p>
+    </div>
+</template>
+<style scoped>
+.fmp-card {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+    padding: 35px 40px;
+    background-color: var(--card-background-primary-color);
+}
+
+.fmp-card .img-controller {
+    width: 72px;
+}
+
+.fmp-card h5 {
+    font-size: 16px;
+    line-height: 24px;
+    letter-spacing: 0.1px;
+    color: var(--dark-primary-text-color);
+}
+
+.fmp-card p {
+    font-size: 14px;
+    line-height: 20px;
+    letter-spacing: 0.2px;
+    color: var(--dark-secondary-text-color);
+}
+
+.fmp-card .line-separator {
+    width: 50px;
+    height: 2px;
+    border: 1px solid var(--line-separator-primary-color);
+}
+</style>
